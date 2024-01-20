c384ea779c8be3ba8c1d480a8f2078cb1feedc9b
Rajkumar Natarajan <raj.cs.psg@gmail.com>
2024-01-19 23:49:59 -0800

    neotree setup similar to loctvl

--- lua/lualine-config/init.lua
+++ lua/lualine-config/init.lua
@@ -1,3 +1,63 @@
-require('lualine').setup({
+--[=====[require('lualine').setup({
   options = {theme = 'tokyodark', options = {section_separators = '', component_separators = ''}}
-})
+})]=====]-- 
+
+local config = require("lualine-config.config")
+
+local M = {}
+
+local function setup()
+  local cpn = require("lualine-config.components")
+  local theme = require("lualine-config.highlights").custom(config.options)
+
+  require("lualine").setup({
+    options = {
+      theme = theme,
+      icons_enabled = true,
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      disabled_filetypes = {
+        statusline = { "dashboard", "lazy", "alpha" },
+      },
+      ignore_focus = {},
+      always_divide_middle = true,
+      globalstatus = true,
+      refresh = {
+        statusline = 1000,
+        tabline = 1000,
+        -- winbar = 100,
+      },
+    },
+    sections = {
+      lualine_a = { cpn.branch },
+      lualine_b = { cpn.diagnostics },
+      lualine_c = {},
+      lualine_x = { cpn.diff },
+      lualine_y = { cpn.position, cpn.filetype },
+      lualine_z = { cpn.spaces, cpn.mode },
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { "filename" },
+      lualine_x = { "location" },
+      lualine_y = {},
+      lualine_z = {},
+    },
+    tabline = {},
+    extensions = {},
+  })
+end
+
+M.setup = config.setup
+
+M.load = function()
+  setup()
+  vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = function()
+      setup()
+    end,
+  })
+end
+
+return M
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -15,12 +15,21 @@ local plugins = {
   },--]=====]
 
   'nvim-tree/nvim-web-devicons',
-  {
+  --[=====[{
     'kyazdani42/nvim-tree.lua',
     dependenciess = 'kyazdani42/nvim-web-devicons',
     config = function()require'nvim-tree'.setup {} end
-  },
-
+  }]=====]--,
+{
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+}, 
   {'windwp/nvim-autopairs'},
 
   {'folke/which-key.nvim'},
