e158b9df83c9a19f512bf00664f7305276d7215e
Rajkumar Natarajan <raj.cs.psg@gmail.com>
2024-08-15 20:34:56 -0700

    remove unnecessary config

--- lua/whichkey-config/init.lua
+++ lua/whichkey-config/init.lua
@@ -3,11 +3,12 @@ local wk = require("which-key")
 local mappings = {
   q = {":q<cr>", "Quit"},
   Q = {":wq<cr>", "Save and Quit"},
-  w = {":w<cr>", "Save"},
+--  w = {":w<cr>", "Save"},
   x = {":bdelete<cr>", "Close"},
-  E = {":e ~/.config/nvim/init.lua<cr>", "Edit Config"},
-  f = {":Telescope find_files<cr>", "Telescope Find Files"},
-  r = {":Telescope live_grep<cr>", "Telescope Live Grep"}
+--  E = {":e ~/.config/nvim/init.lua<cr>", "Edit Config"},
+--  f = {":Telescope find_files<cr>", "Telescope Find Files"},
+--  r = {":Telescope live_grep<cr>", "Telescope Live Grep"}
 }
+
 local opts = { prefix = '<leader>'}
 wk.register(mappings, opts)
