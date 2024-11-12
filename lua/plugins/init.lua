c05ac8255845b279b6234e7a67226a59c331b82d
Rajkumar Natarajan <rajkumar.natarajan@ekata.com>
2024-11-11 17:58:02 -0800

    added symbols.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -79,6 +79,18 @@ local plugins = {
 		end,
 	},
 	{
+		"oskarrrrrrr/symbols.nvim",
+		config = function()
+			local r = require("symbols.recipes")
+			require("symbols").setup(r.DefaultFilters, r.AsciiSymbols, {
+				-- custom settings here
+				-- e.g. hide_cursor = false
+			})
+			vim.keymap.set("n", ",s", ":Symbols<CR>")
+			vim.keymap.set("n", ",S", ":SymbolsClose<CR>")
+		end,
+	},
+	{
 		"cuducos/yaml.nvim",
 		ft = { "yaml" }, -- optional
 		dependencies = {
