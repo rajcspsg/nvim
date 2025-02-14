2eda86a2e2bb99bc7d947f2f7f9780b653c5075f
Rajkumar Natarajan <rajkumar.natarajan@ekata.com>
2025-02-13 22:59:03 -0800

    fixed some errors

--- lua/plugins/lsp-config/init.lua
+++ lua/plugins/lsp-config/init.lua
@@ -1,9 +1,9 @@
-local old = {
+return {
 	require("plugins/lsp-config/misc"),
 	require("plugins/lsp-config/lspkind-config"),
-	require("plugins/lsp-config/language_servers"),
 	require("plugins/lsp-config/clojure"),
 	require("plugins/lsp-config/yaml-config"),
+	require("plugins/lsp-config/go"),
+	require("plugins/lsp-config/rust"),
+	require("plugins/lsp-config/language_servers"),
 }
-
-return old
