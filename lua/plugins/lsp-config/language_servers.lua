54f634914ef7d19e9a3c213479e9c04fbd0b7cb2
Rajkumar Natarajan <rajkumar.natarajan@ekata.com>
2025-03-14 20:02:39 -0700

    added rust debugger support

--- lua/plugins/lsp-config/language_servers.lua
+++ lua/plugins/lsp-config/language_servers.lua
@@ -47,13 +47,11 @@ return {
       "dhall_lsp_server",
       "elixirls",
       "emmet_ls",
-      "golangci_lint_ls",
       "gopls",
       "gradle_ls",
       "hls",
       "html",
       "lua_ls",
-      "metals",
       "nixd",
       "ocamlls",
       "pylsp",
@@ -79,27 +77,17 @@ return {
       settings = {
 
         Lua = {
-
           diagnostics = {
-
             globals = { "vim" },
           },
-
           workspace = {
-
             library = {
-
               [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-
               [vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
-
               [vim.fn.stdpath("data") .. "/lazy/ui/nvchad_types"] = true,
-
               [vim.fn.stdpath("data") .. "/lazy/lazy.nvim/lua/lazy"] = true,
             },
-
             maxPreload = 100000,
-
             preloadFileSize = 10000,
           },
         },
--- lua/plugins/lsp-config/rust.lua
+++ lua/plugins/lsp-config/rust.lua
@@ -1,63 +1,63 @@
 return {
-	{
-		"mrcjkb/rustaceanvim",
-		version = "^5", -- Recommended
-		lazy = false, -- This plugin is already lazy
-		["rust-analyzer"] = {
-			assist = {
-				importEnforceGranularity = true,
-				importPrefix = "create",
-			},
-			cargo = {
-				allFeatures = true,
-				loadOutDirsFromCheck = true,
-				runBuildScripts = true,
-			},
-			checkOnSave = {
-				-- default: `cargo check`
-				command = "clippy",
-				allFeatures = true,
-			},
-			procMacro = {
-				enable = true,
-			},
-			inlayHints = {
-				lifetimeElisionHints = {
-					enable = true,
-					useParameterNames = true,
-				},
-			},
-		},
-	},
-	{
-		"saecki/crates.nvim",
-		tag = "stable",
-		event = { "BufRead Cargo.toml" },
-		config = function()
-			require("crates").setup({
-				completion = {
-					cmp = {
-						enabled = true,
-					},
-					crates = {
-						enabled = true,
-						max_results = 8,
-						min_chars = 3,
-					},
-				},
-				lsp = {
-					enabled = true,
-					actions = true,
-					completion = true,
-					hover = true,
-				},
-			})
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5", -- Recommended
+    lazy = false,   -- This plugin is already lazy
+    ["rust-analyzer"] = {
+      assist = {
+        importEnforceGranularity = true,
+        importPrefix = "create",
+      },
+      cargo = {
+        allFeatures = true,
+        loadOutDirsFromCheck = true,
+        runBuildScripts = true,
+      },
+      checkOnSave = {
+        -- default: `cargo check`
+        command = "clippy",
+        allFeatures = true,
+      },
+      procMacro = {
+        enable = true,
+      },
+      inlayHints = {
+        lifetimeElisionHints = {
+          enable = true,
+          useParameterNames = true,
+        },
+      },
+    },
+  },
+  {
+    "saecki/crates.nvim",
+    tag = "stable",
+    event = { "BufRead Cargo.toml" },
+    config = function()
+      require("crates").setup({
+        completion = {
+          cmp = {
+            enabled = true,
+          },
+          crates = {
+            enabled = true,
+            max_results = 8,
+            min_chars = 3,
+          },
+        },
+        lsp = {
+          enabled = true,
+          actions = true,
+          completion = true,
+          hover = true,
+        },
+      })
 
-			local cmp = require("cmp")
-			local cfg = cmp.get_config()
-			table.insert(cfg, { name = "crates" })
-			cmp.setup(cfg)
-		end,
-	},
-	{ "cordx56/rustowl", dependencies = { "neovim/nvim-lspconfig" } },
+      local cmp = require("cmp")
+      local cfg = cmp.get_config()
+      table.insert(cfg, { name = "crates" })
+      cmp.setup(cfg)
+    end,
+  },
+  { "cordx56/rustowl", dependencies = { "neovim/nvim-lspconfig" } },
 }
