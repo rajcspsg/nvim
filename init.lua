26d6860b12647181a3c3c902b12147940317b724
Rajkumar Natarajan <rajkumar.natarajan@ekata.com>
2025-03-29 21:26:01 -0700

    chore: fix local leader

--- init.lua
+++ init.lua
@@ -1,55 +1,50 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
+
+require("keybindings")
+require("options")
 
 local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 keymap("", " ", "<Nop>", opts)
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = ","
-
-
-
 for _, source in ipairs({
-  "rajnvim.bootstrap",
+	"rajnvim.bootstrap",
 }) do
-  local status_ok, fault = pcall(require, source)
-  if not status_ok then
-    vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
-  end
+	local status_ok, fault = pcall(require, source)
+	if not status_ok then
+		vim.api.nvim_err_writeln("Failed to load " .. source .. "\n\n" .. fault)
+	end
 end
 
-require("keybindings")
-require("options")
-
 require("lazy").setup("plugins", {
-  defaults = { lazy = false },
-  install = { colorscheme = { "tokyonight" } },
-  checker = { enabled = true },
-  concurrency = 10,
-  performance = {
-    rtp = {
-      disabled_plugins = {
-        "gzip",
-        "netrwPlugin",
-        "tarPlugin",
-        "tutor",
-        "zipPlugin",
-      },
-    },
-  },
-  debug = false,
+	defaults = { lazy = false },
+	install = { colorscheme = { "tokyonight" } },
+	checker = { enabled = true },
+	concurrency = 10,
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"netrwPlugin",
+				"tarPlugin",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+	debug = false,
 })
 
 vim.cmd("colorscheme astrodark") -- " Dark theme (default)
